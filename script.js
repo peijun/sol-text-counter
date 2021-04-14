18753130431a674d619df299a9c51cb3fb1bfa78
Jumpei Funayama <contact.zouri.music@gmail.com>
2021-04-14 15:10:48 +0900

    stcの作成

---
+++ script.js
@@ -0,0 +1,31 @@
+let counter = 0
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request == "Action") {
+		hogehoge();
+	}
+});
+
+function hogehoge() {
+    if(counter == 0){    
+        const script = document.createElement('script');
+        const code = '(' + function () {
+            window.addEventListener('textcounter', arg => {
+                var content = window.parent.tinymce.get('submission_body').getContent();
+                var no_html = content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'');
+
+                alert(no_html.length + '文字です');
+            });
+        } + ')()';
+        script.text = code;
+
+        document.head.appendChild(script);
+    
+        window.dispatchEvent(new CustomEvent('textcounter'));
+
+        counter = counter + 1;
+    } else {
+        window.dispatchEvent(new CustomEvent('textcounter',));
+    }
+}
+
